42ee9d1726a1e19cee7c6414d9c1359e4ce04291
Bhardwaj <nakul.bhardwaj@intel.com>
2021-10-13 13:23:59 +0530

    slim branch cloned on 13 oct,2021

---
+++ profile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright (C) 2019 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
+set -a
+
+#this is provided while using Utility OS
+source /opt/bootstrap/functions
+
+
+
+# --- Add Packages
+ubuntu_bundles="openssh-server"
+ubuntu_packages="wget"
+
+# --- List out any docker images you want pre-installed separated by spaces. ---
+pull_sysdockerimagelist=""
+
+# --- List out any docker tar images you want pre-installed separated by spaces.  We be pulled by wget. ---
+wget_sysdockerimagelist="" 
+
+
+
+# --- Install Extra Packages ---
+run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
+    "docker run -i --rm --privileged --name ubuntu-installer ${DOCKER_PROXY_ENV} -v /dev:/dev -v /sys/:/sys/ -v $ROOTFS:/target/root ubuntu:${param_ubuntuversion} sh -c \
+    'mount --bind dev /target/root/dev && \
+    mount -t proc proc /target/root/proc && \
+    mount -t sysfs sysfs /target/root/sys && \
+    LANG=C.UTF-8 chroot /target/root sh -c \
+        \"$(echo ${INLINE_PROXY} | sed "s#'#\\\\\"#g") export TERM=xterm-color && \
+        export DEBIAN_FRONTEND=noninteractive && \
+        ${MOUNT_DURING_INSTALL} && \
+        apt install -y tasksel && \
+        tasksel install ${ubuntu_bundles} && \
+        apt install -y ${ubuntu_packages}\"'" \
+    ${PROVISION_LOG}
+
+# --- Pull any and load any system images ---
+for image in $pull_sysdockerimagelist; do
+	run "Installing system-docker image $image" "docker exec -i system-docker docker pull $image" "$TMP/provisioning.log"
+done
+for image in $wget_sysdockerimagelist; do
+	run "Installing system-docker image $image" "wget -O- $image 2>> $TMP/provisioning.log | docker exec -i system-docker docker load" "$TMP/provisioning.log"
+done
