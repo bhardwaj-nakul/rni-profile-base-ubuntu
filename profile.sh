e0d7a04cbfa38034b48b69349950d700bf3f66b3
Nakul Bhardwaj <nakul.bhardwaj@intel.com>
2021-10-22 13:47:48 +0530

    Desktop branch

--- profile.sh
+++ profile.sh
@@ -8,19 +8,9 @@ set -a
 #this is provided while using Utility OS
 source /opt/bootstrap/functions
 
-
-
-# --- Add Packages
-ubuntu_bundles="openssh-server"
-ubuntu_packages="wget"
-
-# --- List out any docker images you want pre-installed separated by spaces. ---
-pull_sysdockerimagelist=""
-
-# --- List out any docker tar images you want pre-installed separated by spaces.  We be pulled by wget. ---
-wget_sysdockerimagelist="" 
-
-
+# --- Config
+ubuntu_bundles="ubuntu-desktop openssh-server"
+ubuntu_packages="net-tools vim software-properties-common apt-transport-https wget"
 
 # --- Install Extra Packages ---
 run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
@@ -29,18 +19,9 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
     mount -t proc proc /target/root/proc && \
     mount -t sysfs sysfs /target/root/sys && \
     LANG=C.UTF-8 chroot /target/root sh -c \
-        \"$(echo ${INLINE_PROXY} | sed "s#'#\\\\\"#g") export TERM=xterm-color && \
-        export DEBIAN_FRONTEND=noninteractive && \
-        ${MOUNT_DURING_INSTALL} && \
-        apt install -y tasksel && \
-        tasksel install ${ubuntu_bundles} && \
-        apt install -y ${ubuntu_packages}\"'" \
+    \"$(echo ${INLINE_PROXY} | sed "s#'#\\\\\"#g") export TERM=xterm-color && \
+    export DEBIAN_FRONTEND=noninteractive && \
+    apt install -y tasksel && \
+    tasksel install ${ubuntu_bundles} && \
+    apt install -y ${ubuntu_packages}\"'" \
     ${PROVISION_LOG}
-
-# --- Pull any and load any system images ---
-for image in $pull_sysdockerimagelist; do
-	run "Installing system-docker image $image" "docker exec -i system-docker docker pull $image" "$TMP/provisioning.log"
-done
-for image in $wget_sysdockerimagelist; do
-	run "Installing system-docker image $image" "wget -O- $image 2>> $TMP/provisioning.log | docker exec -i system-docker docker load" "$TMP/provisioning.log"
-done
